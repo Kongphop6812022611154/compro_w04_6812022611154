0559038738ef0670863ba1ad7617eb178c8d3984
Kongphop6812022611154 <s6812022611154@email.kmutnb.ac.th>
2025-07-22 16:58:57 +0700

    Update Coding_w04-04(float).c

--- Coding_w04-04(float).c
+++ Coding_w04-04(float).c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    float score = 90.5;
+    float score = 90.5;                // float score ใช้สำหรับเก็บค่าทศนิยม
     printf("a: %f\n", score);
 
     return 0;
 }
+
+//1.score มีค่า = 90.500000
+//2.หากใช้ float จะสามารถตอบเป็นค่าที่มีจุดทศนิยมได้
+//3.สรุปคือ float เหมาะสำหรับผลลัพธ์ค่าที่เป็นจุดทศนิยม
