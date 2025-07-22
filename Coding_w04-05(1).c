e78ed0b2462598fc1b43cca5788dba329b17285f
Kongphop6812022611154 <s6812022611154@email.kmutnb.ac.th>
2025-07-22 17:14:19 +0700

    Update Coding_w04-05(1).c

--- Coding_w04-05(1).c
+++ Coding_w04-05(1).c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
 void countCall() {
-    static int counter = 0;
-    counter++;
-    printf("Call function: counter = %d\n", counter);
+    static int counter = 0;                                    
+    counter++;                                                 
+    printf("Call function: counter = %d\n", counter);         
 }
 
-int main() {
+int main() {                                                   
     printf("Starting function calls...\n");
     countCall();
     printf("After first call:\n");
     countCall();
     return 0;
 }
+
+//ผลลัพธ์ที่ได้คือ Starting function calls...
+//Call function: counter = 1
+//After first call:
+//Call function: counter = 2
+
+
+//Non-static Variable คือเมื่อเวลาเรียกใช้ฟังก์ชั่นจะไม่นำค่าเก่ามาคำนวณ ส่วน static variable คือเมื่อเวลาเรียกใช้ฟังก์ชั่นจะนำค่าเก่ามาคำนวณด้วย
